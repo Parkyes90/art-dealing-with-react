69fb52f757eda4c16ed6fba71046c70aa79938e7
parkyes90 <parkyes90@gmail.com>
2019-01-15 17:15:30 +0900

    todo complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './containers/App';
+import './styles/main.scss'
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 // import { createStore } from "redux";
@@ -14,7 +14,7 @@ import * as serviceWorker from './serviceWorker';
 // );
 
 ReactDOM.render(
-  <div>test</div>,
+  <App />,
   document.getElementById('root')
 );
 
