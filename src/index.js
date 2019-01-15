6f7f79a7f12f91fd58eea0d33f102a3e6ec8fc8e
parkyes90 <parkyes90@gmail.com>
2019-01-15 18:46:06 +0900

    immutable, redux-actions 사용

--- src/index.js
+++ src/index.js
@@ -4,17 +4,29 @@ import './styles/main.scss'
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+import modules from './modules';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  modules,
+  window.__REDUX_DEVTOOLS_EXTENSION__
+  && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 // import { createStore } from "redux";
 // import reducers from './reducers';
 // import { Provider } from 'react-redux';
 
 // const store = createStore(
-  // reducers,
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// reducers,
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 // );
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
